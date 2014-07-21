643dd7617a22913b0ef6529ebcd4a9c7a90b03c5
lcostantini <lcostantini@buntu.(none)>
2014-07-21 12:34:16 -0300

    Notification test

--- test/models/notification_test.rb
+++ test/models/notification_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class NotificationTest < ActiveSupport::TestCase
+
+  def setup
+    @user = User.new
+    @team = Team.new
+  end
+
   test "user notifications" do
+    @notification = Notification.create(@user, @team)
+    @notification.accept
     assert @team.users.include? (@user)
   end
 end
