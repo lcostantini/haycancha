a887e495c6931082dcbfc63189a647a09cb403bf
lcostantini <costantinileandro1@gmail.com>
2014-08-06 09:46:24 -0300

    Fixed problems to access show events

--- app/controllers/users/events_controller.rb
+++ app/controllers/users/events_controller.rb
@@ -1,5 +1,5 @@
 class Users::EventsController < Users::ApplicationController
-  before_action :set_event
+  before_action :set_event, only: [:show]
 
   def index
     @team = Team.find(params[:team_id])
