868ea28786182a72cbdb3a5a1cd339397ca5260f
lcostantini <costantinileandro1@gmail.com>
2014-08-22 12:33:29 -0300

    Change test_helper code climate

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,10 @@
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
   fixtures :all
-
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
-
 end
