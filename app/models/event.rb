1cba5f92a57c36e084c0f3121dd67bc5ae1ba012
lcostantini <costantinileandro1@gmail.com>
2014-08-27 11:27:27 -0300

    Errors in event and team

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,9 +6,11 @@ class Event < ActiveRecord::Base
   scope :future, -> { where('created_for > ?', Time.now) }
   scope :coming_soon, -> { where(created_for: Date.today..2.weeks.from_now) }
   scope :expired, ->{ where('created_for < ?', Time.now) }
-
+  
+  validates :name, presence: true
+  
   after_create :send_invitation!
-
+  
   def send_invitation!
     team.users.each do |user|
       responses.create user: user
--- app/models/team.rb
+++ app/models/team.rb
@@ -5,4 +5,7 @@ class Team < ActiveRecord::Base
 
   has_and_belongs_to_many :users
   has_many :events
+  
+  validates :name, presence: true
+  
 end
