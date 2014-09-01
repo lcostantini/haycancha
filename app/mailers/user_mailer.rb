46855500b8e1d59907860e1c0c51d45b6d4f5613
lcostantini <costantinileandro1@gmail.com>
2014-09-01 11:21:09 -0300

    Seting environment variable for email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,14 @@ class UserMailer < ActionMailer::Base
 
   def invited_email(user, current_user)
     @current_user_email = current_user.email
-    @url = 'http://localhost:3000/'
+    @url = ENV["URL_EMAIL"]
     mail(to: user.email, subject: 'Invitation to HayCancha')
   end
 
   def waiting_email(event, user)
     @event = event
     @user = user
-    @url = 'http://localhost:3000/'
+    @url = ENV["URL_EMAILS"]
     mail(to: @user.email, subject: 'Reminder for a Event')
   end
 
