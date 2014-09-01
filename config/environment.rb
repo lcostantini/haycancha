c72ed5472e5a2484954c19c6bf40b38ccd4103ae
lcostantini <costantinileandro1@gmail.com>
2014-09-01 13:54:53 -0300

    Config mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'haycancha.herokuapp.com',
+  user_name:            ENV["USR_NAME"],
+  password:             ENV["PSW_EMAIL"],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
