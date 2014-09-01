06839cc97c356ef38c012845c3ae597c6c174353
lcostantini <costantinileandro1@gmail.com>
2014-09-01 14:03:07 -0300

    Fix enviroment error

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,9 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'haycancha.herokuapp.com',
+  domain:               'gmail.com',
   user_name:            ENV["USR_NAME"],
   password:             ENV["PSW_EMAIL"],
   authentication:       'plain',
   enable_starttls_auto: true  }
+  
