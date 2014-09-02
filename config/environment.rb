4c3247564b6d1d44c3c50472904f704a8e3a621b
lcostantini <costantinileandro1@gmail.com>
2014-09-02 09:22:43 -0300

    Config action mailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'gmail.com',
-  user_name:            ENV["USR_NAME"],
-  password:             ENV["PSW_EMAIL"],
-  authentication:       'plain',
-  enable_starttls_auto: true  }
-  
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,5 +80,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: ENV["URL_EMAIL"] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV["USR_NAME"],
+    password:             ENV["PSW_EMAIL"],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
 end
